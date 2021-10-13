3dd3d690e0445cf66951e33c7f07b79cceee4bdb
Serega <solodovnikov.sf@edu.spbstu.ru>
2021-10-13 13:05:07 +0300

    get horses links added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from selenium import webdriver
 from fake_useragent import UserAgent
-import time
+from time import sleep
 import os
 
+from moves.get_horses_links import get_horses_links
+from moves.go_to_horselist import go_to_horselist
 from moves.login import login
 
 
@@ -11,6 +13,7 @@ def get_options(user_agent):
     options = webdriver.ChromeOptions()
     options.add_argument(f"user-agent={user_agent.chrome}")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--start-maximized")
 
     return options
 
@@ -23,8 +26,14 @@ if __name__ == '__main__':
     options = get_options(user_agent)
 
     browser = webdriver.Chrome(
-        executable_path=r"D:\Projects\lowbot\chromedriver.exe",
+        executable_path=r"C:\Users\Sergey\PycharmProjects\lowbot\chromedriver.exe",
         options=options
     )
 
     login(browser)
+    sleep(2)
+    go_to_horselist(browser)
+    sleep(2)
+    hrefs = get_horses_links(browser)
+    sleep(1)
+    print(hrefs)
