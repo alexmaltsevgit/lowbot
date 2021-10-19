ee88d5448985cabc1cb0a93ddf3e5d95364b1608
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-19 22:10:45 +0300

    added functionality to chrome wrapper

--- browser/WrappedChrome.py
+++ browser/WrappedChrome.py
@@ -11,6 +11,12 @@ class WrappedChrome(webdriver.Chrome):
     click_sleep_time = 0.5
     field_fill_sleep_time = 0.5
 
+    def find(self, css_selector):
+        return self.find_element_by_css_selector(css_selector)
+
+    def find_all(self, css_selector):
+        return self.find_elements_by_css_selector(css_selector)
+
     def click_on(self, element, sleep_after: int = click_sleep_time):
         element = self._convert_to_web_element(element)
         element.click()
@@ -35,7 +41,7 @@ class WrappedChrome(webdriver.Chrome):
         sleep(sleep_after)
 
     def fill_many_fields(self, fields_and_values: dict, sleep_after: int = field_fill_sleep_time):
-        for field, value in fields_and_values:
+        for field, value in fields_and_values.items():
             self.fill_field(field, value, sleep_after)
 
     def wait_for(self, css_selector):
@@ -46,6 +52,14 @@ class WrappedChrome(webdriver.Chrome):
         )
 
         return el
+
+    def open_new_tab(self):
+        old_tab = self.current_window_handle
+        self.execute_script("window.open();")
+        self.switch_to.window(self.window_handles[1])
+        new_tab = self.current_window_handle
+
+        return old_tab, new_tab
 
     def _convert_to_web_element(self, el) -> WebElement:
         if type(el) is str:
