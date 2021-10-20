343c1d3b89d983e7ae0db6544d6c0430ac07921d
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-20 20:12:24 +0300

    added parse_get_parameters function

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,6 +3,16 @@ from typing import Callable
 from browser.WrappedChrome import WrappedChrome
 
 
+def parse_get_parameters(url: str) -> dict:
+    parameters = url.split('?')[1]
+    parameters = parameters.split('&')
+    parameters = map(lambda parameter: parameter.split('='), parameters)
+    parameters = list(parameters)
+    parameters = {parameter[0]: parameter[1] for parameter in parameters}
+
+    return parameters
+
+
 def in_new_tab(driver: WrappedChrome, callback: Callable, url: str = ""):
     old_tab, new_tab = driver.open_new_tab(url)
     callback()
