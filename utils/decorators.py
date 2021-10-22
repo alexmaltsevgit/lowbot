6aa887db6d808fc2f16fa0358b5b6b5cc18f775c
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-22 19:43:07 +0300

    added decorators

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,11 +1,51 @@
 import time
 
 
+def decorate_all_methods(decorator, exclude=None):
+    if exclude is None:
+        exclude = []
+
+    def decorate(cls):
+        for attr in cls.__dict__:
+            if callable(getattr(cls, attr)) and attr not in exclude:
+                setattr(cls, attr, decorator(getattr(cls, attr)))
+        return cls
+
+    return decorate
+
+
 def sleep_after(sleep_time):
     def decorator(function):
         def wrapper(*args, **kwargs):
-            function(*args, **kwargs)
+            res = function(*args, **kwargs)
             time.sleep(sleep_time)
+            return res
+
+        return wrapper
+
+    return decorator
+
+
+def return_default_on_exception(default, exception=Exception):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except exception:
+                return default
+
+        return wrapper
+
+    return decorator
+
+
+def pass_exception(exception=Exception):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except exception:
+                pass
 
         return wrapper
 
