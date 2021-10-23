3cc9ec3574d5a8a99ebf2b696f3f53033660ff06
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-23 17:27:52 +0300

    fixed ksk sign up

--- lowadi/pages/Horse.py
+++ lowadi/pages/Horse.py
@@ -3,24 +3,24 @@ from selenium.webdriver.common.by import By
 import lowadi
 
 
-class Horse(lowadi.Page):
+class Horse(lowadi.Page, lowadi.HasID):
     url: str = "https://www.lowadi.com/elevage/chevaux/cheval?id={id}"
     selectors = {
         "id": {
             "owner": "#ownerBoite",
         },
-        "link_part": {
-            "ksk": "elevage/chevaux/centreInscription?id=",
-        },
+        "css": {
+            "ksk_link": "a[href*=\"centreInscription\"]"
+        }
     }
 
     def proceed(self):
         pass
 
     def needs_ksk(self):
-        ksk_link = Horse.selectors['link_part']['ksk']
+        ksk_link = Horse.selectors['css']['ksk_link']
         return bool(
-            self.site.find_element(By.PARTIAL_LINK_TEXT, ksk_link)
+            self.site.find_element(By.CSS_SELECTOR, ksk_link)
         )
 
     def is_proceedable(self):
