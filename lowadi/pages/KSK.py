f5c73c423b6599a3b64760b6c1906a0f397e4395
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-23 17:27:52 +0300

    fixed ksk sign up

--- lowadi/pages/KSK.py
+++ lowadi/pages/KSK.py
@@ -3,21 +3,24 @@ from selenium.webdriver.common.by import By
 import lowadi
 
 
-class KSK(lowadi.Page):
+class KSK(lowadi.Page, lowadi.HasID):
     url = "https://www.lowadi.com/elevage/chevaux/centreInscription?id={id}"
     selectors = {
-        "css": {
-            "ksk_60_days_tab": "#table-0 > thead > tr > .caption-module[colspan=5] > .grid-table > .grid-row > "
-                               "span.grid-cell:last-child > a",
-            "ksk_60_days_btn": "#table-0 > tbody > tr:first-child > td:nth-child(11) > button"
+        "xpath": {
+            "ksk_60_days_tab": "/html/body/div[7]/main/section/section/div[1]/table/thead/tr/td[6]/span[2]/span/span["
+                               "9]/a",
+            "ksk_60_days_btn": "/html/body/div[7]/main/section/section/div[1]/table/tbody/tr[1]/td[10]/button"
         }
     }
 
     def sign_up(self):
-        ksk_60_days_tab = KSK.selectors['css']['ksk_60_days_tab']
-        ksk_60_days_tab = self.site.find_element(By.CSS_SELECTOR, ksk_60_days_tab)
+        ksk_60_days_tab = KSK.selectors['xpath']['ksk_60_days_tab']
+        ksk_60_days_tab = self.site.find_element(By.XPATH, ksk_60_days_tab)
         self.site.click_on(ksk_60_days_tab)
 
-        ksk_60_days_btn = KSK.selectors['css']['ksk_60_days_btn']
-        ksk_60_days_btn = self.site.find_element(By.CSS_SELECTOR, ksk_60_days_btn)
+        ksk_60_days_btn = KSK.selectors['xpath']['ksk_60_days_btn']
+        ksk_60_days_btn = self.site.find_element(By.XPATH, ksk_60_days_btn)
         self.site.click_on(ksk_60_days_btn)
+
+        alert = self.site.switch_to.alert
+        alert.accept()
